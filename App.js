2b3cbd5b9e089b3014ab0cc1e85bec64859b7c9c
Avi Kasliwal <avi.kasliwal04@gmail.com>
2022-07-23 12:48:53 +0530

    Using Google Fonts

--- App.js
+++ App.js
@@ -3,7 +3,29 @@ import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.sc
 
 import { theme } from "./src/infrastructure/theme";
 
+import {
+  useFonts as oswaldUseFonts,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+
+import {
+  useFonts as latoUseFonts,
+  Lato_400Regular,
+} from "@expo-google-fonts/lato";
+
 export default function App() {
+  const [oswaldLoaded] = oswaldUseFonts({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = latoUseFonts({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
